5e6ac7d6c9444888e3a20d19ca5b7cb1f08687c5
Ruiqin Li <r5li@ucsd.edu>
2022-03-02 11:22:50 -0800

    Update run.py

--- run.py
+++ run.py
@@ -15,63 +15,62 @@ def main(targets):
         trips = pd.read_csv('data/trips.csv')
         utility = pd.read_csv('data/utilityvars.csv')
 
-        # drop columns with no significant values (e.g. only 1 value, only unique values)
-        utility = utility.drop(columns=['activityid', 'tourmode'])
+        sample1 = utility.sample(n=1000000, random_state=1)
+        size = 500000
+        mode1 = utilityvars[utilityvars['targettripmode']==1].sample(n=int(round(size*props[1])), random_state=1)
+        mode2 = utilityvars[utilityvars['targettripmode']==2].sample(n=int(round(size*props[2])), random_state=1)
+        mode3 = utilityvars[utilityvars['targettripmode']==3].sample(n=int(round(size*props[3])), random_state=1)
+        mode4 = utilityvars[utilityvars['targettripmode']==4].sample(n=int(round(size*props[4])), random_state=1)
+        mode5 = utilityvars[utilityvars['targettripmode']==5].sample(n=int(round(size*props[5])), random_state=1)
+        mode6 = utilityvars[utilityvars['targettripmode']==6].sample(n=int(round(size*props[6])), random_state=1)
+        mode7 = utilityvars[utilityvars['targettripmode']==7].sample(n=int(round(size*props[7])), random_state=1)
+        mode8 = utilityvars[utilityvars['targettripmode']==8].sample(n=int(round(size*props[8])), random_state=1)
+        mode9 = utilityvars[utilityvars['targettripmode']==9].sample(n=int(round(size*props[9])), random_state=1)
+        mode10 = utilityvars[utilityvars['targettripmode']==10].sample(n=int(round(size*props[10])), random_state=1)
+        mode11 = utilityvars[utilityvars['targettripmode']==11].sample(n=int(round(size*props[11])), random_state=1)
+        mode12 = utilityvars[utilityvars['targettripmode']==12].sample(n=int(round(size*props[12])), random_state=1)
+        sample2 = pd.concat([mode1,mode2,mode3,mode4,mode5,mode6,mode7,mode8,mode9,mode10,mode11,mode12], axis=0)
 
-        utility['gender'] = np.where(utility['gender'] == True, 'female', 'male')
-        utility['autosuf'] = np.where(utility['autosuf'] == 0, 'no_vehicles', np.where(utility['autosuf'] == 1, 'insufficient', 'sufficient'))
-        utility['tourpurpose'] = np.where(utility['tourpurpose'] == 0, 'work', np.where(utility['tourpurpose'] == 1, 'university', np.where(utility['tourpurpose'] == 2, 'school', np.where(utility['tourpurpose'] == 3, 'maintenance', np.where(utility['tourpurpose'] == 4, 'escort', np.where(utility['tourpurpose'] == 5, 'discretionary', np.where(utility['tourpurpose'] == 6, 'work-based', 'cross-border')))))))
+        new_utilityvars1 = pd.get_dummies(sample1, columns=["tourpurpose"], prefix=["tourpurpose"])
+        new_utilityvars2 = pd.get_dummies(sample2, columns=["tourpurpose"], prefix=["tourpurpose"])
 
-        Y = utility['targettripmode'] - 1
-        X1 = utility.select_dtypes(include=['object','bool'])
-        X2 = utility.select_dtypes(exclude=['object','bool']).drop(columns='targettripmode')
+        seed = 7
+        test_size = 0.33
 
-        expanded_data = []
-        for col in X1.columns:
-            dummies = pd.get_dummies(X1[col], prefix=col)
-            expanded_data.append(dummies)
+        new_utilityvars1 = pd.merge(new_utilityvars1.drop(['targettripmode', 'tourmode', 'activityid'], axis=1), new_utilityvars1[['targettripmode']], left_index=True, right_index=True, how="outer")
+        df_array1 = new_utilityvars1.values
+        X1 = df_array1[:,0:34]
+        Y1 = df_array1[:,34]
+        X_train1, X_test1, y_train1, y_test1 = train_test_split(X1, Y1, test_size=test_size, random_state=seed)
+        model1 = xgb.XGBClassifier()
+        model1.fit(X_train1, y_train1, eval_set = [(X_train1, y_train1), (X_test1, y_test1)], verbose = 0)
+        y_pred1 = [round(value) for value in model1.predict(X_test1)]
+        y_test1 = [round(value) for value in y_test1]
 
-        expanded_data = pd.concat(expanded_data, axis=1)
+        new_utilityvars2 = pd.merge(new_utilityvars2.drop(['targettripmode', 'tourmode', 'activityid'], axis=1), new_utilityvars2[['targettripmode']], left_index=True, right_index=True, how="outer")
+        df_array2 = new_utilityvars2.values
+        X2 = df_array2[:,0:34]
+        Y2 = df_array2[:,34]
+        X_train2, X_test2, y_train2, y_test2 = train_test_split(X2, Y2, test_size=test_size, random_state=seed)
+        model2 = xgb.XGBClassifier()
+        model2.fit(X_train2, y_train2, eval_set = [(X_train2, y_train2), (X_test2, y_test2)], verbose = 0)
+        y_pred2 = [round(value) for value in model2.predict(X_test2)]
+        y_test2 = [round(value) for value in y_test2]
 
-        data_encoded = pd.concat([expanded_data, X2, Y], axis=1)
-
-        mode1 = data_encoded[data_encoded['targettripmode']==0].sample(n=1000, random_state=1)
-        mode2 = data_encoded[data_encoded['targettripmode']==1].sample(n=1000, random_state=1)
-        mode3 = data_encoded[data_encoded['targettripmode']==2].sample(n=1000, random_state=1)
-        mode4 = data_encoded[data_encoded['targettripmode']==3].sample(n=1000, random_state=1)
-        mode5 = data_encoded[data_encoded['targettripmode']==4].sample(n=1000, random_state=1)
-        mode6 = data_encoded[data_encoded['targettripmode']==5].sample(n=1000, random_state=1)
-        mode7 = data_encoded[data_encoded['targettripmode']==6].sample(n=1000, random_state=1)
-        mode8 = data_encoded[data_encoded['targettripmode']==7].sample(n=1000, random_state=1)
-        mode9 = data_encoded[data_encoded['targettripmode']==8].sample(n=1000, random_state=1)
-        mode10 = data_encoded[data_encoded['targettripmode']==9].sample(n=1000, random_state=1)
-        mode11 = data_encoded[data_encoded['targettripmode']==10].sample(n=1000, random_state=1)
-        mode12 = data_encoded[data_encoded['targettripmode']==11].sample(n=1000, random_state=1)
-
-        combined_subsamples = pd.concat([mode1,mode2,mode3,mode4,mode5,mode6,mode7,mode8,mode9,mode10,mode11,mode12], axis=0)
-
-        data_array = combined_subsamples.values
-        X = data_array[:, 0:(data_array.shape[1]-1)]
-        Y = data_array[:,(data_array.shape[1]-1)]
-
-        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
-
-        xgb_model = XGBClassifier(objective='multi:softprob',eval_metric='auc',num_class=12,use_label_encoder=False)
-        xgb_model.fit(X_train, y_train, eval_set=[(X_train,y_train),(X_test,y_test)],verbose=0)
-        y_pred = xgb_model.predict(X_test)
-        y_pred = [round(value) for value in y_pred]
-
-        accuracy = metrics.accuracy_score(y_test, y_pred)
-        sensitivity = metrics.recall_score(y_test, y_pred, average = 'macro')
-        precision = metrics.precision_score(y_test, y_pred, average = 'macro')
+        accuracy1 = metrics.accuracy_score(y_test1, y_pred1)
+        sensitivity = metrics.recall_score(y_test1, y_pred1, average = 'macro')
+        precision = metrics.precision_score(y_test1, y_pred1, average = 'macro')
         f1 = (2 * precision * sensitivity) / (precision + sensitivity)
-        cf_matrix = metrics.confusion_matrix(y_test, y_pred)
+        cf_matrix = metrics.confusion_matrix(y_test1, y_pred1)
+        accuracy2 = metrics.accuracy_score(y_test2, y_pred2)
 
         print("The evaluation metric stats")
-        print("Accuracy: %.2f%%" % (accuracy * 100.0))
+        print("Accuracy: %.2f%%" % (accuracy1 * 100.0))
         print("Sensitivity: %.2f%%" % (sensitivity * 100.0))
         print("Precision: %.2f%%" % (precision * 100.0))
         print("F1: %.2f%%" % (f1 * 100.0))
+
+        model1.save_model("model1.json")
 
 if __name__ == '__main__':
     targets = sys.argv[1:]
